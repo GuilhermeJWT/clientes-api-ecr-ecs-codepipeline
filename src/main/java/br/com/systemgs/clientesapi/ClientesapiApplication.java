3aba648064971217c3501031839cadf1c3a89261
GuilhermeJWT <guiromanno@gmail.com>
2024-10-10 16:44:27 -0300

    ADD: Projeto Clientes Api

---
+++ src/main/java/br/com/systemgs/clientesapi/ClientesapiApplication.java
@@ -0,0 +1,12 @@
+package br.com.systemgs.clientesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesapiApplication.class, args);
+	}
+}
